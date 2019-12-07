f37123db107ad4b24d1a964efc289fa350600ab7
jodreclado <rogersx@uw.edu>
2019-12-06 20:18:06 -0800

    Updates

---
+++ precache-manifest.30957a0aa063b18d89bc0e45357d5782.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e906dee0d14d4be6a1eef719b394c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d95fffee4d6670ad93b",
+    "url": "/static/css/main.adb56ecd.chunk.css"
+  },
+  {
+    "revision": "de6010cbb9fdeed58a1b",
+    "url": "/static/js/2.cb75bdd9.chunk.js"
+  },
+  {
+    "revision": "99024a1901fda55340ed",
+    "url": "/static/js/3.7241df61.chunk.js"
+  },
+  {
+    "revision": "561a6431bba584c935f2",
+    "url": "/static/js/4.fb786c07.chunk.js"
+  },
+  {
+    "revision": "f74b459cd12b8205b0f8",
+    "url": "/static/js/5.b5a8ff78.chunk.js"
+  },
+  {
+    "revision": "5d95fffee4d6670ad93b",
+    "url": "/static/js/main.be5ba7e3.chunk.js"
+  },
+  {
+    "revision": "a818b7c2d3a6a0c2b3ef",
+    "url": "/static/js/runtime~main.6da055d8.js"
+  },
+  {
+    "revision": "aaa002a1b679d0f4228e3f18054219f6",
+    "url": "/static/media/background.aaa002a1.png"
+  }
+]);
