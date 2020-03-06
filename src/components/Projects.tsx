5fbfc5ca39bc31547bf22c39f2d20b276205c3fc
jodreclado <rogersx@uw.edu>
2020-03-06 00:35:28 -0800

    Add placeholder projects page.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,19 @@
 import React, { useEffect } from 'react';
 
+// pictures (3:2 aspect ratios)
+import photonic from '../assets/images/photonic.jpg';
+import creature from '../assets/images/creature.jpg';
+import baroque from '../assets/images/baroque.jpg';
+import medical from '../assets/images/medical.jpg';
+import risc from '../assets/images/risc.jpg';
+import interlock from '../assets/images/interlock.jpg';
+
+const imgStyle = {
+  "width": "90%",
+  "maxHeight": "60%",
+  "object-fit": "cover",
+};
+
 const Projects = () => {
   useEffect(() => {
     document.title = "Projects";
@@ -8,7 +22,52 @@ const Projects = () => {
   return (
     <div className='page'>
       <h2>Projects</h2>
-      <p>Undergoing renovation...</p>
+      <div className="row">
+        <div className="column">
+          <img src={photonic} alt="Industry capstone" style={imgStyle}></img>
+          <h3>Photonic Sentry</h3>
+          <div className="content">
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+        <div className="column">
+          <img src={creature} alt="Embedded capstone" style={imgStyle}></img>
+          <h3>Creatures</h3>
+          <div className="content">
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+        <div className="column">
+          <img src={baroque} alt="Ultima agent" style={imgStyle}></img>
+          <h3>Baroque Chess Agent</h3>
+          <div className="content">
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+        <div className="column">
+          <img src={medical} alt="Medical system" style={imgStyle}></img>
+          <h3>Medical Monitoring System</h3>
+          <div className="content">
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          <img src={risc} alt="Microprocessor" style={imgStyle}></img>
+          <h3>RISC Microprocessor</h3>
+          <div className="content">
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+        <div className="column">
+          <img src={interlock} alt="Interlock system" style={imgStyle}></img>
+          <h3>Interlock System</h3>
+          <div className="content">
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
